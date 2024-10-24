7e88e164a3c3051fe2590f9999d0703918c9ac66
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-10-24 22:39:24 +0200

    KAN-25: Changed the design of the navbar appropiate to mockup

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewModeService } from '../view-mode.service';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  protected viewModeService = inject(ViewModeService);
+
+  switchToCardMode() {
+    this.viewModeService.switchToCardMode();
+  }
+  switchToStationMode() {
+    this.viewModeService.switchToStationMode();
+  }
 
 }
