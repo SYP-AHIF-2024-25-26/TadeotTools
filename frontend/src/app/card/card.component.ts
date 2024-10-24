f069b24a690799516783a6d44d9c662d9a325ae8
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-10-24 22:37:11 +0200

    KAN-25: Made card state saveable and added some styles

--- frontend/src/app/card/card.component.ts
+++ frontend/src/app/card/card.component.ts
@@ -1,17 +1,35 @@
-import { Component, Input, signal } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  computed,
+  inject,
+  Input,
+  signal,
+  ViewChild,
+  WritableSignal,
+} from '@angular/core';
 import { Card } from '../main-page/main-page.component';
 import { CheckboxComponent } from '../checkbox/checkbox.component';
-
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [
     CheckboxComponent,
+    CommonModule,
   ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
   @Input() card!: Card;
+  @Input() id!: string;
+  @Input() bgColor!: string;
+  @ViewChild (CheckboxComponent) checkbox!: CheckboxComponent;
+
+  isChecked() {
+    return this.checkbox?.isChecked() ?? false;
+  }
 
 }
