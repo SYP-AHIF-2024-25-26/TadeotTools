b2af4bacf9073f1f46f9f775b3d9daa818454d2b
HaasLuca <114491239+HaasLuca@users.noreply.github.com>
2024-11-02 18:58:36 +0100

    KAN-24 CreateStopStats implemented

--- backend/API/Controllers/StopStatisticsController.cs
+++ backend/API/Controllers/StopStatisticsController.cs
@@ -1,14 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using TadeoT.Database;
+using TadeoT.Database.Functions;
 
 namespace API.Controllers;
 
 [ApiController]
-[Route("v1/api/stops/groups")]
+[Route("v1/api/stops/")]
 public class GroupStopsController : ControllerBase
 {
-    [HttpGet("{groupId}")]
-    public IActionResult GetStopsByGroupId(int groupId)
-    {
+    [HttpPost]
+    [Route("stats/{stopId}")]
+    public IActionResult CreateStopStats(int stopId) {
+        StopStatisticFunctions.GetInstance().AddStopStatistic(new StopStatistic() {
+            IsDone = true,
+            StopID = stopId,
+            Time = DateTime.Now,
+            Stop = StopFunctions.GetInstance().GetStopById(stopId)
+        });
+    }
+
+    [HttpPut("stats/{stopId}")]
+    public IActionResult UpdateStopStats(int stopId, [FromBody] object stats) {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("stats/{stopId}")]
+    public IActionResult DeleteStopStats(int stopId) {
         throw new NotImplementedException();
     }
 }
