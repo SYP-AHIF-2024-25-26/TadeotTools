a49f2a5fe0618a677f7a017d41ee5ea7b8b8ae34
HaasLuca <114491239+HaasLuca@users.noreply.github.com>
2024-11-02 19:54:40 +0100

    KAN-24 removed useless classes

--- backend/API/DTOs/StopGroupDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace API.DTOs;
-
-public class StopGroupDTO {
-    public required string Name { get; set; }
-    public required string Description { get; set; }
-    public required string Color { get; set; }
-}
