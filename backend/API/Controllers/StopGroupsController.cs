8210af8ebe3865da24a5a88622464e9868aae535
HaasLuca <114491239+HaasLuca@users.noreply.github.com>
2024-11-02 18:57:32 +0100

    KAN-24 StopGroupsController implemented

--- backend/API/Controllers/StopGroupsController.cs
+++ backend/API/Controllers/StopGroupsController.cs
@@ -1,55 +1,79 @@
-using System.Text.RegularExpressions;
 using API.DTOs;
 using Microsoft.AspNetCore.Mvc;
-using TadeoT.Database.Functions;
 using TadeoT.Database;
+using TadeoT.Database.Functions;
 
 namespace API.Controllers;
 
 [ApiController]
 [Route("v1/groups")]
-public class StopGroupsController : ControllerBase
-{
+public class StopGroupsController : ControllerBase {
     [HttpGet]
     public IActionResult GetGroups() {
         try {
             return Ok(StopGroupFunctions.GetInstance().GetAllStopGroups());
         }
-        catch (Exception e) {
+        catch (TadeoTDatabaseException) {
             return StatusCode(500, "internal server error");
         }
     }
 
     [HttpGet("{groupId}")]
-    public IActionResult GetGroupById(int groupId)
-    {
+    public IActionResult GetGroupById(int groupId) {
         try {
             return Ok(StopGroupFunctions.GetInstance().GetStopsOfStopGroup(groupId));
         }
-        catch (TadeoTDatabaseException e) {
+        catch (TadeoTDatabaseException) {
             return StatusCode(500, $"Could not get stops with groupID {groupId}");
         }
     }
 
     [HttpPost]
-    public IActionResult CreateGroup([FromBody] StopGroupDTO group) {
-        StopGroupFunctions.GetInstance().AddStopGroup(new StopGroup {
-            Name = group.Name,
-            Description = group.Description,
-            Color = group.Color,
-        });
-        throw new NotImplementedException();
+    public IActionResult CreateGroup([FromBody] StopGroupDTO? group) {
+        try {
+            StopGroupFunctions.GetInstance().AddStopGroup(new StopGroup {
+                Name = group!.Name,
+                Description = group.Description,
+                Color = group.Color,
+            });
+            return Ok();
+        }
+        catch (TadeoTDatabaseException) {
+            return StatusCode(500, $"Could not create group {group!.Name}");
+        }
+        catch (NullReferenceException) {
+            return StatusCode(406, "Missing group data");
+        }
     }
 
     [HttpPut("{groupId}")]
-    public IActionResult UpdateGroup(int groupId, [FromBody] object group)
-    {
-        throw new NotImplementedException();
+    public IActionResult UpdateGroup([FromBody] StopGroup? group) {
+        try {
+            if (group == null) {
+                return StatusCode(406, "Missing group data");
+            }
+            StopGroupFunctions.GetInstance().UpdateStopGroup(group);
+            return Ok();
+        }
+        catch (TadeoTDatabaseException) {
+            return StatusCode(500, $"Could not update group {group!.Name}");
+        }
     }
 
     [HttpDelete("{groupId}")]
-    public IActionResult DeleteGroup(int groupId)
-    {
-        throw new NotImplementedException();
+    public IActionResult DeleteGroup(int groupId) {
+        try {
+            try {
+                StopGroupFunctions.GetInstance().GetStopGroupById(groupId);
+            }
+            catch (TadeoTDatabaseException) {
+                return StatusCode(404, "No group found");
+            }
+            StopGroupFunctions.GetInstance().DeleteStopGroupById(groupId);
+            return Ok();
+        }
+        catch (TadeoTDatabaseException) {
+            return StatusCode(500, "Could not delete group");
+        }
     }
 }
