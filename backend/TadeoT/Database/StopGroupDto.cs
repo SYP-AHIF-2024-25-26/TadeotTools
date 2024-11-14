20210e26c140e4fe49e8eb7522e460fbc063ec47
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-11-14 18:05:25 +0100

    deleted dto inside db

--- backend/TadeoT/Database/StopGroupDto.cs
+++
@@ -1,8 +0,0 @@
-namespace API;
-
-public class StopGroupDto {
-    public int StopGroupID { get; set; }
-    public required String Name { get; set; }
-    public required string Description { get; set; }
-    public required string Color { get; set; }
-}
