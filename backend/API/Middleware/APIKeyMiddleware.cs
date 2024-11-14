65de24f01e93aacdcc23ec45b06db4ee911fe70b
HaasLuca <l.haas@students.htl-leonding.ac.at>
2024-11-14 12:02:04 +0100

    KAN-50 Added ApiKey Middelware

---
+++ backend/API/Middleware/APIKeyMiddleware.cs
@@ -0,0 +1,59 @@
+using System.Security.Cryptography;
+using System.Text;
+using TadeoT.Database;
+using TadeoT.Database.Functions;
+
+namespace API.Middleware;
+
+public class ApiKeyMiddleware
+{
+    private readonly RequestDelegate _next;
+    private const string ApiKeyHeaderName = "X-Api-Key";
+
+    public ApiKeyMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Only apply to specific routes, e.g., /api/products
+        if (context.Request.Path.ToString().Contains("/api"))
+        {
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401; // Unauthorized
+                await context.Response.WriteAsync("API Key was not provided.");
+                return;
+            }
+
+            if (!IsValidApiKey(extractedApiKey))
+            {
+                context.Response.StatusCode = 403; // Forbidden
+                await context.Response.WriteAsync("Invalid API Key.");
+                return;
+            }
+        }
+
+        await _next(context);
+    }
+
+    private bool IsValidApiKey(string userApiKey)
+    {
+        List<string> systemApiKeys = APIKeyFunctions.GetInstance().GetAllAPIKeys().Select(ak => ak.APIKeyValue).ToList();
+        
+        byte[] userApiKeyBytes = Encoding.UTF8.GetBytes(userApiKey);
+        
+        bool result = false;
+        
+        systemApiKeys.ForEach(systemApiKey => {
+            byte[] systemApiKeyBytes = Encoding.UTF8.GetBytes(systemApiKey);
+
+            if (CryptographicOperations.FixedTimeEquals(userApiKeyBytes, systemApiKeyBytes))
+            {
+                result = true;
+            }
+        });
+        return result;
+    }
+}
