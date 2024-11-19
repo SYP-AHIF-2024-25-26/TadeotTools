a229e20acf321498f13d55c639361451a3df5a20
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-19 16:33:46 +0100

    changed main page component

--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -3,10 +3,7 @@ import { GuideCardComponent } from '../guide-card/guide-card.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeaderComponent } from '../header/header.component';
 import { ViewModeService } from '../view-mode.service';
-import { DescriptionModalComponent } from '../description-modal/description-modal.component';
-import { ModalViewService } from '../modal-view.service';
 import { GuideCard } from '../types';
-import { StopGroupName } from '../stop-group-name.enum';
 
 @Component({
   selector: 'app-main-page',
@@ -15,23 +12,21 @@ import { StopGroupName } from '../stop-group-name.enum';
     GuideCardComponent,
     NavbarComponent,
     HeaderComponent,
-    DescriptionModalComponent
 ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
   protected viewModeService = inject(ViewModeService);
-  protected modalViewService = inject(ModalViewService);
 
 
   cards = signal<GuideCard[]>([
-    { id: 1, title: 'Kurzpräsentationen', description: 'Test 1', isTour: false },
-    { id: 2, title: 'Informatik Medientechnik - Tour', description: 'Test 2', isTour: true, stopGroupNames: [StopGroupName.Informatik, StopGroupName.Medientechnik, StopGroupName.Neutral] },
-    { id: 3, title: 'Elektronik Medizintechnik - Tour', description: 'Test 3', isTour: true, stopGroupNames: [StopGroupName.Elektronik, StopGroupName.Medizintechnik, StopGroupName.Neutral] },
-    { id: 4, title: 'Aula Highlights', description: 'Test 4', isTour: false },
-    { id: 5, title: 'Beratungszentrum', description: 'Test 5', isTour: false },
-    { id: 6, title: 'Voranmeldung', description: 'Test 6', isTour: false },
+    { id: 1, title: 'Kurzpräsentationen', description: 'Test 1' },
+    { id: 2, title: 'Informatik Medientechnik - Tour', description: 'Test 2' },
+    { id: 3, title: 'Elektronik Medizintechnik - Tour', description: 'Test 3' },
+    { id: 4, title: 'Aula Highlights', description: 'Test 4' },
+    { id: 5, title: 'Beratungszentrum', description: 'Test 5' },
+    { id: 6, title: 'Voranmeldung', description: 'Test 6' },
   ]);
 
 
