a0ee27b80cde4f592c4f8a1543c8eb3a02672182
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-19 16:29:35 +0100

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { TourPageComponent } from './tour-page/tour-page.component';
+import { StopPageComponent } from './stop-page/stop-page.component';
 
 export const routes: Routes = [
   { path: 'main', component: MainPageComponent },
-  { path: 'tour', component: TourPageComponent },
+  { path: 'tour/:id', component: StopPageComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
