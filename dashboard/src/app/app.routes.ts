ff3a8686ba590580505655ff600b52967425b875
HaasLuca <l.haas@students.htl-leonding.ac.at>
2024-11-22 10:58:48 +0100

    KAN-19 added tailwind

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import {StopgroupsComponent} from "./stopgroups/stopgroups.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'stopgroups', component: StopgroupsComponent},
   {path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
 
