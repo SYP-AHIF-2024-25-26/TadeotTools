70dcf8c081c78e7c7392664482d076ec0e8dde34
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-22 21:47:32 +0100

    changed routes and config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, InjectionToken, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -8,7 +8,7 @@ export const BASE_URL = new InjectionToken<string>('BaseUrl');
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     { provide: BASE_URL, useValue: 'http://localhost:5000' },
     provideHttpClient(withFetch()),
   ]
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { StopPageComponent } from './stop-page/stop-page.component';
+import { StopDescriptionPageComponent } from './stop-description-page/stop-description-page.component';
 
 export const routes: Routes = [
   { path: 'main', component: MainPageComponent },
-  { path: 'tour/:id', component: StopPageComponent },
+  { path: 'tour/:stopGroupId', component: StopPageComponent },
+  { path: 'tour/:stopGroupId/stop/:stopId', component: StopDescriptionPageComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
