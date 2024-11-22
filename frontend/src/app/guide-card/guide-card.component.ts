e8429460edd754b121d981bbcb26885cc872d57b
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-22 21:49:50 +0100

    Adjusted Guide Card for new progress detection

--- frontend/src/app/guide-card/guide-card.component.ts
+++ frontend/src/app/guide-card/guide-card.component.ts
@@ -1,11 +1,12 @@
 import {
-  Component, inject,
-  Input, ViewChild,
+  Component, EventEmitter, inject,
+  Input, Output, signal, ViewChild, WritableSignal,
 } from '@angular/core';
 import { CheckboxComponent } from '../checkbox/checkbox.component';
 import { CommonModule } from '@angular/common';
 import { StopGroup } from '../types';
 import { Router } from '@angular/router';
+import { GUIDE_CARD_PREFIX, MANUAL_CHECK_PREFIX, STOP_GROUP_PROGRESS_PREFIX, STOPS_COUNT_PREFIX } from '../constants';
 
 @Component({
   selector: 'app-guide-card',
@@ -21,35 +22,51 @@ export class GuideCardComponent {
   @Input() stopGroup!: StopGroup;
   @Input() id!: string;
   @ViewChild(CheckboxComponent) checkbox!: CheckboxComponent;
-
+  @Output() openStopPage = new EventEmitter<void>();
   protected router = inject(Router);
+  progress: WritableSignal<number | null> = signal(null);
+  stopsCount: WritableSignal<number | null> = signal(null);
 
-  //progress = computed(() => this.calculateProgress());
-  // max = computed(() => this.calculateMax());
-
-  /*private calculateProgress(): number | null {
-    const IDs = sessionStorage.getItem(this.stopGroupNames.join(','));
-    if (IDs === null) {
-      return null;
-    }
-    const stopIDs = JSON.parse(IDs) as string[];
-    return stopIDs.filter(id => sessionStorage.getItem(id) === 'true').length;
+  ngAfterViewInit() {
+    this.initializeProgressAndCount();
+    this.checkManualCheck();
   }
-
-  private calculateMax(): number | null {
-    const IDs = sessionStorage.getItem(this.stopGroupNames.join(','));
-    if (IDs === null) {
-      return null;
-    }
-    return JSON.parse(IDs).length;
-  }
-  */
 
   isChecked(): boolean {
     return this.checkbox?.isChecked() ?? false;
   }
 
-  openStopPage() {
-    this.router.navigate([`tour/${this.stopGroup.stopGroupID}`], )
+  private initializeProgressAndCount() {
+    this.progress.set(this.getProgress());
+    this.stopsCount.set(this.getStopsCount());
+  }
+
+  private checkManualCheck() {
+    const manualCheck = sessionStorage.getItem(MANUAL_CHECK_PREFIX + this.id);
+    if (manualCheck !== null) {
+      this.checkbox.isChecked.set(manualCheck === 'true');
+    }
+    this.updateSessionStorage();
+  }
+
+  private updateSessionStorage() {
+    if (this.progress() !== null && this.stopsCount() !== null) {
+      if (this.progress() === this.stopsCount()) {
+        sessionStorage.setItem(GUIDE_CARD_PREFIX + this.id, 'true');
+        sessionStorage.removeItem(MANUAL_CHECK_PREFIX + this.id);
+      } else if (sessionStorage.getItem(MANUAL_CHECK_PREFIX + this.id) !== 'true') {
+        sessionStorage.setItem(GUIDE_CARD_PREFIX + this.id, 'false');
+      }
+    }
+  }
+
+  private getProgress(): number | null {
+    const progress = sessionStorage.getItem(STOP_GROUP_PROGRESS_PREFIX + this.stopGroup.stopGroupID);
+    return progress !== null ? Number(progress) : null;
+  }
+
+  private getStopsCount(): number | null {
+    const count = sessionStorage.getItem(STOPS_COUNT_PREFIX + this.stopGroup.stopGroupID);
+    return count !== null ? Number(count) : null;
   }
 }
