d545d161c79b3b521f6b16b77dbbae52ff7d040b
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-22 21:52:55 +0100

    Adjusted Stop Page Component to Code Refactoring

--- frontend/src/app/stop-page/stop-page.component.ts
+++ frontend/src/app/stop-page/stop-page.component.ts
@@ -1,28 +1,65 @@
-import { Component, computed, inject, Input, Signal } from '@angular/core';
+import { Component, inject, Input, signal, ViewChildren, WritableSignal } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { NavbarComponent } from "../navbar/navbar.component";
 import { BreadcrumbComponent } from "../breadcrumb/breadcrumb.component";
-import { GuideCardComponent } from '../guide-card/guide-card.component';
 import { ApiFetchService } from '../api-fetch.service';
-import { Stop } from '../types';
+import { Division, Stop, StopGroup } from '../types';
 import { StopCardComponent } from '../stop-card/stop-card.component';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { DescriptionContainerComponent } from '../description-container/description-container.component';
+import {
+  CURRENT_STOP_GROUP_PREFIX,
+  CURRENT_STOP_PREFIX,
+  STOP_GROUP_PROGRESS_PREFIX,
+  STOPS_COUNT_PREFIX,
+} from '../constants';
 
 @Component({
   selector: 'app-stop-page',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent, BreadcrumbComponent, StopCardComponent],
+  imports: [HeaderComponent, NavbarComponent, BreadcrumbComponent, StopCardComponent, NgClass, DescriptionContainerComponent],
   templateUrl: './stop-page.component.html',
   styleUrl: './stop-page.component.css'
 })
 export class StopPageComponent {
   protected apiFetchService = inject(ApiFetchService);
   protected router = inject(Router);
-  private route = inject(ActivatedRoute)
-  id: Signal<string | null> = computed(() => {
-    return this.route.snapshot.paramMap.get('id');
-  })
-  tour: Stop[] = [];
+  @Input({ required: true }) stopGroupId!: string;
+  @ViewChildren(StopCardComponent) stopCards!: StopCardComponent[];
+  parentStopGroup: WritableSignal<StopGroup> = signal({} as StopGroup);
+  stops: WritableSignal<Stop[]> = signal([]);
+  divisions: WritableSignal<Division[]> = signal([]);
 
+  async ngOnInit() {
+    if (this.stopGroupId === undefined) {
+      await this.router.navigate(['/']);
+    }
+    else {
+      await this.onLoad();
+    }
+  }
 
+  async onLoad() {
+    if (sessionStorage.getItem(CURRENT_STOP_GROUP_PREFIX) !== null) {
+      this.parentStopGroup.set(JSON.parse(sessionStorage.getItem(CURRENT_STOP_GROUP_PREFIX)!) as StopGroup);
+    }
+    this.stops.set(await this.apiFetchService.getStopsOfGroup(Number(this.stopGroupId)!));
+    this.divisions.set(await this.apiFetchService.getDivisions());
+  }
+
+  getColorOfStop(stop: Stop) {
+    return this.divisions().find(division => division.divisionID === stop.divisionID)?.color;
+  }
+
+  async openStopDescriptionPage(stop: Stop) {
+    sessionStorage.setItem(CURRENT_STOP_PREFIX, JSON.stringify(stop));
+    await this.router.navigate(['/tour', this.parentStopGroup().stopGroupID, 'stop', stop.stopID]);
+  }
+
+  setProgress() {
+    const progress = this.stopCards.filter(stopCard => stopCard.isChecked()).length;
+    sessionStorage.setItem(STOP_GROUP_PROGRESS_PREFIX + this.parentStopGroup().stopGroupID, progress.toString());
+    sessionStorage.setItem(STOPS_COUNT_PREFIX + this.parentStopGroup().stopGroupID, this.stops().length.toString());
+  }
 }
