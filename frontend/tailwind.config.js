09214749b7c73ca3d8c0bca8c03c668a97a6dc63
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-22 21:53:38 +0100

    changed the font to roboto google font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         'electronics-red': '#CE1223',
         'medicin-technology-yellow': '#f1a102',
         'neutral-green': '#80c076',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
       }
     },
   },
