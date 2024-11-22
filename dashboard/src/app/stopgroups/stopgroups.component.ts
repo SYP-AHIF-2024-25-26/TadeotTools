b4d41a589f27b440a3ccf23166b57b5e89ac1bd5
HaasLuca <l.haas@students.htl-leonding.ac.at>
2024-11-22 09:08:34 +0100

    KAN-53 added stopgroups component

---
+++ dashboard/src/app/stopgroups/stopgroups.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stopgroups',
+  standalone: true,
+  imports: [],
+  templateUrl: './stopgroups.component.html',
+  styleUrl: './stopgroups.component.css'
+})
+export class StopgroupsComponent {
+
+}
