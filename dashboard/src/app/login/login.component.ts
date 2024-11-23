97f66da09a186fc955d5d368bdc605b47f56e34b
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-11-23 21:27:00 +0100

    KAN-67 added login and colors

--- dashboard/src/app/login/login.component.ts
+++ dashboard/src/app/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  apiKey = signal<string>("");
+  constructor(private router: Router) {
+    this.apiKey.set(localStorage.getItem('apiKey') || '');
+    if (this.apiKey() !== '') {
+      this.saveApiKey();
+    }
+  }
 
+  saveApiKey() {
+    if (this.apiKey() !== '') {
+      localStorage.setItem('apiKey', this.apiKey());
+      this.router.navigate(['/stopgroups']);
+    }
+  }
 }
--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}" // Ensure it includes your Angular files
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'informatic-blue': '#0059A7',
+        'media-technology-blue': '#70B4D9',
+        'electronics-red': '#CE1223',
+        'medicin-technology-yellow': '#f1a102',
+        'neutral-green': '#80c076',
+        'htl-orange': '#e8704f',
+        'htl-orange-dark': '#b4573d',
+      }
+    },
   },
   plugins: [],
 }
