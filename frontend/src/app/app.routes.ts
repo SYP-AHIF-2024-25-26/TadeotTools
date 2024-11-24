57fdaafe5fdf6dc2a4a9a97941117d9a2866e77a
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-24 16:55:42 +0100

    KAN-38: Added routes for Map component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { StopPageComponent } from './stop-page/stop-page.component';
 import { StopDescriptionPageComponent } from './stop-description-page/stop-description-page.component';
+import { MapComponent } from './map/map.component';
+import { StopPageComponent } from './stop-page/stop-page.component';
 
 export const routes: Routes = [
   { path: 'main', component: MainPageComponent },
   { path: 'tour/:stopGroupId', component: StopPageComponent },
   { path: 'tour/:stopGroupId/stop/:stopId', component: StopDescriptionPageComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'map/:roomNr', component: MapComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
