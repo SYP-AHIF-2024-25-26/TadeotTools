0476aaf3088a6e744a00e03ca86e6f41aab078a1
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2024-11-24 22:50:04 +0100

    KAN-38: Added go back button

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,28 +1,18 @@
 import { Component, computed, Input, signal } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-map',
   standalone: true,
   templateUrl: './map.component.html',
   styleUrl: './map.component.css',
-  imports: [
-    HeaderComponent,
-    NavbarComponent,
-    NgOptimizedImage,
-  ],
+  imports: [HeaderComponent, NavbarComponent],
 })
 export class MapComponent {
   @Input() roomNr: string | undefined;
 
-   images = [
-    '/assets/stockwerk-U.png',
-    '/assets/stockwerk-E.png',
-    '/assets/stockwerk-1.png',
-     '/assets/stockwerk-2.png',
-   ];
+  images = ['/assets/stockwerk-U.png', '/assets/stockwerk-E.png', '/assets/stockwerk-1.png', '/assets/stockwerk-2.png'];
 
   currentFloor = signal(1);
   currentFloorSymbol = computed(() => {
@@ -31,8 +21,7 @@ export class MapComponent {
 
   ngOnInit() {
     if (this.roomNr) {
-      this.currentFloor.set(this.images
-        .findIndex(image => image.includes(this.roomNr!.charAt(0))));
+      this.currentFloor.set(this.images.findIndex((image) => image.includes(this.roomNr!.charAt(0))));
     }
   }
 
@@ -41,5 +30,9 @@ export class MapComponent {
     if (newFloor >= 0 && newFloor < this.images.length) {
       this.currentFloor.set(newFloor);
     }
+  }
+
+  onGoBack() {
+    window.history.back();
   }
 }
