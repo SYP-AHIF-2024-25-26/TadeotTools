92371e4f53758bc8b431ca009e1327f0ec6f784a
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-11-24 14:27:48 +0100

    fix api key functions

--- backend/TadeoT/Database/Functions/APIKeyFunctions.cs
+++ backend/TadeoT/Database/Functions/APIKeyFunctions.cs
@@ -3,14 +3,9 @@ using TadeoT.Database.Model;
 
 namespace TadeoT.Database.Functions;
 
-public class APIKeyFunctions
+public class APIKeyFunctions(TadeoTDbContext context)
 {
-    private readonly TadeoTDbContext context;
-
-    public APIKeyFunctions(TadeoTDbContext context)
-    {
-        this.context = context;
-    }
+    private readonly TadeoTDbContext context = context;
 
     public async Task<List<APIKey>> GetAllAPIKeys()
     {
@@ -25,6 +20,10 @@ public class APIKeyFunctions
 
     public async Task<APIKey> AddAPIKey(APIKey apiKey)
     {
+        if (apiKey == null)
+        {
+            throw new TadeoTArgumentNullException("Could not add APIKey because it was null");
+        }
         try
         {
             this.context.APIKeys.Add(apiKey);
@@ -36,7 +35,7 @@ public class APIKeyFunctions
         }
     }
 
-    public async void DeleteAPIKey(APIKey apiKey)
+    public async Task DeleteAPIKey(APIKey apiKey)
     {
         try
         {
