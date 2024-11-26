1817e12bdbeb94c3ac46d6d2d798539f04e7cb89
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-11-26 15:09:22 +0100

    KAN-52 add stop detail

---
+++ dashboard/src/app/stop-details/stop-details.component.ts
@@ -0,0 +1,79 @@
+import { Component, inject, signal } from '@angular/core';
+import { StopService } from '../stop.service';
+import { BASE_URL } from '../app.config';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Division } from '../types';
+import { DivisionService } from '../division.service';
+
+@Component({
+  selector: 'app-stop-details',
+  standalone: true,
+  imports: [FormsModule, RouterModule],
+  templateUrl: './stop-details.component.html',
+  styleUrl: './stop-details.component.css'
+})
+export class StopDetailsComponent {
+  private service: StopService = inject(StopService);
+  private divisionService: DivisionService = inject(DivisionService);
+  baseUrl = inject(BASE_URL);
+
+  stopId = signal<number>(-1);
+  name = signal<string>('');
+  description = signal<string>('');
+  roomNr = signal<string>('');
+  stopGroupId = signal<number | null>(null);
+  divisionId = signal<number>(5); // general as default
+
+  divisions = signal<Division[]>([]);
+
+  newStop = () => this.stopId() === -1;
+
+  errorMessage = signal<string | null>(null);
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.loadDivisions();
+    this.route.queryParams.subscribe(params => {
+      this.stopId.set(params['id'] || -1);
+      this.name.set(params['name'] || '');
+      this.description.set(params['description'] || '');
+      this.roomNr.set(params['roomNr'] || '');
+      this.stopGroupId.set(params['stopGroupID'] || null);
+    });
+    this.divisionService.getDivisions().then(divisions => {
+      this.divisions.set(divisions);
+    });
+  }
+
+  ngOnInit() {}
+  async loadDivisions() {
+    this.divisions.set(await this.divisionService.getDivisions());
+  }
+
+  async submitStopDetail() {
+    if (this.newStop()) {
+      await this.service.addStop({
+        name: this.name(),
+        description: this.description(),
+        roomNr: this.roomNr(),
+        divisionID: this.divisionId()
+      });
+    } else {
+      await this.service.updateStop({
+        stopID: this.stopId(),
+        name: this.name(),
+        description: this.description(),
+        roomNr: this.roomNr(),
+        stopGroupID: this.stopGroupId() === -1 ? null : this.stopGroupId(),
+        divisionID: this.divisionId()
+      });
+    }
+    this.router.navigate(['/divisions']);
+  }
+
+  async deleteAndGoBack() {
+    await this.service.deleteStop(this.stopId());
+    this.router.navigate(['/divisions']);
+  }
+}
