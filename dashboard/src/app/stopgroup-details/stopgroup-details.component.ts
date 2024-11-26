98e98bb6c55404562e48cc2fd49741e6c1b9e5e5
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-11-26 15:11:30 +0100

    KAN-53 add stopgroup component

---
+++ dashboard/src/app/stopgroup-details/stopgroup-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stopgroup-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './stopgroup-details.component.html',
+  styleUrl: './stopgroup-details.component.css'
+})
+export class StopgroupDetailsComponent {
+
+}
