60500e58a6f2bbc0f4b70e3c4e1f6618bcd69423
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-12-04 13:40:15 +0100

    KAN-67 fixed login

--- dashboard/src/app/login/login.component.ts
+++ dashboard/src/app/login/login.component.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { Router } from '@angular/router';
-import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, NavbarComponent],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -14,9 +13,6 @@ export class LoginComponent {
   apiKey = signal<string>("");
   constructor(private router: Router) {
     this.apiKey.set(localStorage.getItem('API_KEY') || '');
-    if (this.apiKey() !== null) {
-      this.saveApiKey();
-    }
   }
 
   saveApiKey() {
